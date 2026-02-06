3240c7a5140314fb7d8d2187e45df393f9c04187
Robert Dekovich <rob@altoal.com>
2026-02-05 20:51:41 -0600

    refactor(exchange): rename traits module

--- src/core/exchange/mod.rs
+++ src/core/exchange/mod.rs
@@ -29,7 +29,7 @@ mod filter;
 pub mod polymarket;
 mod reconnecting;
 mod scorer;
-mod traits;
+mod exchange_config;
 
 pub use approval::{ApprovalResult, ApprovalStatus, TokenApproval};
 pub use dedup::{DedupConfig, DedupStrategy, MessageDeduplicator};
@@ -37,7 +37,7 @@ pub use factory::ExchangeFactory;
 pub use filter::{MarketFilter, MarketFilterConfig};
 pub use reconnecting::ReconnectingDataStream;
 pub use scorer::MarketScorer;
-pub use traits::ExchangeConfig;
+pub use exchange_config::ExchangeConfig;
 
 // === Trait definitions ===
 
